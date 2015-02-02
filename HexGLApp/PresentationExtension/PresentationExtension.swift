039a6806fa3b127edffdcef6939cb094f17d17a9
Jonathan Dong <jonathan.dong@intel.com>
2015-02-02 17:02:52 +0800

    Updated presentation extension.
    Rename the js file according to the new XWalkView's requirement,
    and inject it into the auto-generated stub codes.
    
    Imported old implementation codes. will make them work soon.

--- HexGLApp/PresentationExtension/PresentationExtension.swift
+++ HexGLApp/PresentationExtension/PresentationExtension.swift
@@ -6,4 +6,121 @@ import Foundation
 import XWalkView
 
 public class PresentationExtension: XWalkExtension {
+
+    var remoteWindows: Array<UIWindow> = []
+    var remoteViewController: RemoteViewController?
+
+    override init() {
+        super.init()
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: "screenDidConnect", name: UIScreenDidConnectNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self,
+            selector: "screenDidDisconnect", name: UIScreenDidDisconnectNotification, object: nil)
+    }
+
+    public override func didGenerateStub(stub: String!) -> String! {
+        var bundle = NSBundle(forClass: self.dynamicType)
+        var className = NSStringFromClass(self.dynamicType)
+        if (className == nil) {
+            return stub
+        }
+
+        if countElements(className.pathExtension) > 0 {
+            className = className.pathExtension
+        }
+        if let path = bundle.pathForResource(className, ofType: "js") {
+            if let content = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil) {
+                for var index = stub.endIndex;
+                    index != stub.startIndex;
+                    index = index.predecessor() {
+                        if stub[index.predecessor()] == "}" {
+                            return stub.substringToIndex(index.predecessor()) + content + stub.substringFromIndex(index.predecessor())
+                        }
+                }
+                return stub.stringByAppendingString(content)
+            }
+        }
+        return stub
+    }
+
+    func createWindowForScreen(screen: UIScreen) -> UIWindow {
+        var window: UIWindow? = nil
+        for win in remoteWindows {
+            if win.screen == screen {
+                window = win
+            }
+        }
+        if window == nil {
+            window = UIWindow(frame: screen.bounds)
+            window?.screen = screen
+            self.remoteWindows.append(window!)
+        }
+        return window!
+    }
+
+    func addViewControllerToWindow(controller: UIViewController, window: UIWindow) {
+        window.rootViewController = controller
+        window.hidden = false
+    }
+
+    func screenDidConnect(notification: NSNotification) {
+        if let screen = notification.object as? UIScreen {
+            println("screenDidConnect")
+            /*
+            var window = createWindowForScreen(screen)
+            var viewController = RemoteViewController()
+            addViewControllerToWindow(viewController, window: window)
+            */
+        }
+    }
+
+    func screenDidDisconnect(notification: NSNotification) {
+        if let screen = notification.object as? UIScreen {
+            println("screenDidDisconnect")
+            /*
+            for var i = 0; i < remoteWindows.count; ++i {
+            if remoteWindows[i].screen == screen {
+            remoteWindows.removeAtIndex(i)
+            return
+            }
+            }
+            */
+        }
+    }
+
+    func jsfunc_requestShow(requestId: NSNumber, url: String, baseUrl: String) {
+        println("js_requestShow called, with requestId:\(requestId), url:\(url), baseUrl:\(baseUrl)")
+
+        var screens = UIScreen.screens()
+        /*
+        for obj in screens {
+        var window = createWindowForScreen(obj as UIScreen)
+        var viewController = RemoteViewController(baseUrl: baseUrl, url: url)
+        addViewControllerToWindow(viewController, window: window)
+        }
+        */
+        var screen: UIScreen = UIScreen.screens()[1] as UIScreen
+        var window = createWindowForScreen(screen)
+        var controller = RemoteViewController()
+        addViewControllerToWindow(controller, window: window)
+
+        controller.loadURL(NSURL(string: baseUrl.stringByAppendingPathComponent(url))!)
+        self.remoteViewController = controller
+
+        var event = [
+            "type": "ShowSucceeded",
+            "requestId": requestId,
+            "data": 1
+        ]
+        super.invokeJavaScript(".dispatchEvent", arguments: [event])
+    }
+
+    func jsfunc_postMessage(viewId: NSNumber, message: String, scope: String) {
+        println("js_postMessage with viewId:\(viewId), message:\(message), scope:\(scope)")
+        remoteViewController?.sendMessage(message)
+    }
+
+    func jsfunc_close(viewId: NSNumber) {
+    }
+
 }
--- HexGLApp/PresentationExtension/RemoteViewController.swift
+++ HexGLApp/PresentationExtension/RemoteViewController.swift
@@ -4,6 +4,68 @@
 
 import Foundation
 import UIKit
+import WebKit
 
-class RemoteViewController: UIViewController {
+class RemoteViewController: UIViewController, WKNavigationDelegate {
+    var webview: WKWebView?
+    var messages: Array<String> = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var userContentController = WKUserContentController()
+
+        var config : WKWebViewConfiguration = WKWebViewConfiguration()
+        config.allowsInlineMediaPlayback = true
+        config.mediaPlaybackRequiresUserAction = false
+        config.userContentController = userContentController
+        var webview = WKWebView(frame: view.frame, configuration: config)
+        webview.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        webview.frame = view.frame
+        webview.navigationDelegate = self
+        view.addSubview(webview)
+
+        self.webview = webview
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func loadURL(url: NSURL) {
+        webview?.loadRequest(NSURLRequest(URL: url))
+    }
+
+    func loadExtension(name: String) {
+        //webview?.loadExtension(name)
+    }
+
+    func sendMessage(message: String) {
+        var jsCodes = "(function() {" +
+            "var e = document.createEvent('Event');" +
+            "e.initEvent('message', true, true);" +
+            "e.data = JSON.stringify(\(message));" +
+            "window.dispatchEvent(e);" +
+        "}())"
+        if webview!.loading {
+            messages.append(jsCodes)
+        } else {
+            evaluateJavaScript(jsCodes)
+        }
+    }
+
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        for jsCodes in messages {
+            evaluateJavaScript(jsCodes)
+        }
+        messages.removeAll()
+    }
+
+    private func evaluateJavaScript(jsCodes: String) {
+        webview?.evaluateJavaScript(jsCodes, completionHandler: { (obj, error) -> Void in
+            if error != nil {
+                println("Failed to inject script: \(jsCodes), with error: \(error)")
+            }
+        })
+    }
 }
